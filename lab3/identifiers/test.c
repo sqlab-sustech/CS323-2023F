e482bf47a3ef37c83af6384a9b4e52a61de0fbb2
Yepang Liu <liuyp1@sustech.edu.cn>
2023-09-25 14:35:59 +0800

    lab3 and spl spec

---
+++ lab3/identifiers/test.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int a = 0;
+    int BBA = a_;
+    int _;
+    int 3a;
+    if (a0 == 0) _ = b0;
+    else _= -b0;
+    while(b != 0) {b1 = b0 - b2;}
+    char c = 'a';
+    return 0;
+}
