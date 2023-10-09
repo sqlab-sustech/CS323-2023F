39c7112d8933eba1b8881b4480818685f9a2cb27
Yepang Liu <liuyp1@sustech.edu.cn>
2023-10-09 15:24:31 +0800

    lab4

---
+++ lab4/comment_sanitizer/test.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main() {
+  printf("Hello world\n"); // print a message
+}
---
+++ lab4/nested_quotation_marks/test.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+  printf("This is a string literal without nested quotation marks");
+  printf("And God said, \"Let there be light,\" and there was light.");
+}
